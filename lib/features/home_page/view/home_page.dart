f00f0316b7d6a788f5568572abb55c5df8d008b5
Young-dog <maxim_terekhin@bk.ru>
2023-05-27 21:46:44 +0300

    New Bottom bar

--- lib/features/home_page/view/home_page.dart
+++ lib/features/home_page/view/home_page.dart
@@ -1,39 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:resourse_app/features/profile/view/profile_screen.dart';
 import 'package:resourse_app/features/reels/view/reels_screen.dart';
-
-import '../../bottom_nav_bar/view/bottom_nav_bar.dart';
 import '../../posts/view/posts_screen.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   static const String id = 'home_page';
 
   const HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  final PageController _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('username'),
+
+    List<Widget> buildScreens() {
+      return [
+        const PostsScreen(),
+        const ReelsScreen(),
+        const ProfileScreen(),
+      ];
+    }
+
+    PersistentTabController controller;
+
+    controller = PersistentTabController(initialIndex: 0);
+
+    List<PersistentBottomNavBarItem> navBarsItems() {
+      return [
+        PersistentBottomNavBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/home.svg',
+            colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+          ),
+        ),
+        PersistentBottomNavBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/reels.svg',
+            colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+            width: 45,
+            height: 45,
+          ),
+        ),
+        PersistentBottomNavBarItem(
+          icon: SvgPicture.asset(
+            'assets/icons/profile.svg',
+            colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+          ),
+        ),
+      ];
+    }
+
+    return PersistentTabView(
+      context,
+      controller: controller,
+      screens: buildScreens(),
+      items: navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.black,
+      // Default is Colors.white.
+      handleAndroidBackButtonPress: true,
+      // Default is true.
+      resizeToAvoidBottomInset: true,
+      // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true,
+      // Default is true.
+      hideNavigationBarWhenKeyboardShows: true,
+      // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.black54,
       ),
-      body: PageView(
-        physics: NeverScrollableScrollPhysics(),
-        controller: _pageController,
-        children: const [
-          PostsScreen(),
-          ReelsScreen(),
-          ProfileScreen(),
-        ],
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: const ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
       ),
-      bottomNavigationBar: BottomNavBar(pageController: _pageController,),
+      screenTransitionAnimation: const ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+      NavBarStyle.style6, // Choose the nav bar style with this property.
     );
   }
 }
